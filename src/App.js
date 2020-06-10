5d9a091405d9bf375a849a4f453503d72a2118a9
SikandarZameer <46181276+SikandarZameer@users.noreply.github.com>
2020-06-10 13:39:19 +0500

    Custom-Slider

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import CustomSlider from "./Components/CustomSlider";
 export default function App() {
   return (
     <div className="App">
-      <CustomSlider />
+      <CustomSlider label1="Distance" label2="5km" points={30} />
+      <CustomSlider label1="Price" label2="500-1000" points={[20, 50]} />
     </div>
   );
 }
--- src/Components/CustomSlider.jsx
+++ src/Components/CustomSlider.jsx
@@ -1,83 +1,78 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Slider from '@material-ui/core/Slider';
-import Typography from '@material-ui/core/Typography';
-import Tooltip from '@material-ui/core/Tooltip';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Slider from "@material-ui/core/Slider";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 
-
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    width: 300 + theme.spacing(3) * 2,
+    width: 300 + 24 * 2,
+    padding: 24
   },
   margin: {
-    height: theme.spacing(3),
-  },
+    height: theme.spacing(1)
+  }
 }));
-
-function ValueLabelComponent(props) {
-  const { children, open, value } = props;
-
-  return (
-    <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
-      {children}
-    </Tooltip>
-  );
-}
-
-ValueLabelComponent.propTypes = {
-  children: PropTypes.element.isRequired,
-  open: PropTypes.bool.isRequired,
-  value: PropTypes.number.isRequired,
-};
 
 const PrettoSlider = withStyles({
   root: {
-    color: 'blue',
-    height: 4,
+    color: "#a2df77",
+    height: 8
   },
   thumb: {
-    height: 12,
-    width: 12,
-    backgroundColor: '#fff',
-    border: '3px solid currentColor',
-    marginTop: -4,
+    height: 24,
+    width: 24,
+    backgroundColor: "#fff",
+    border: "4px solid currentColor",
+    marginTop: -8,
     marginLeft: -12,
-    '&:focus, &:hover, &$active': {
-      boxShadow: 'inherit',
-    },
+    "&:focus,&:hover,&$active": {
+      boxShadow: "inherit"
+    }
   },
   active: {},
   valueLabel: {
-    left: 'calc(-50% + 4px)',
+    left: "calc(-50% + 4px)"
   },
   track: {
-    height: 4,
-    borderRadius: 2,
+    height: 8,
+    borderRadius: 4
   },
   rail: {
-    height: 4,
-    borderRadius: 2,
-  },
+    height: 8,
+    borderRadius: 4
+  }
 })(Slider);
 
-
-export default function CustomizedSlider() {
+const CustomSlider = ({ label1, label2, points }) => {
   const classes = useStyles();
+  const [value, setValue] = React.useState(points);
 
   return (
     <div className={classes.root}>
       <div className={classes.margin} />
-      <Grid container direction="row" justify="space-between" alignItems="flex-start">
+      <Grid
+        container
+        direction="row"
+        justify="space-between"
+        alignItems="flex-start"
+      >
         <Grid item>
-          <Typography>Distance</Typography>
+          <Typography>{label1}</Typography>
         </Grid>
         <Grid item>
-          <Typography>5km</Typography>
+          <Typography>{label2}</Typography>
         </Grid>
       </Grid>
-      <PrettoSlider valueLabelDisplay="auto" aria-label="pretto slider" defaultValue={20} />
+      <PrettoSlider
+        valueLabelDisplay="auto"
+        defaultValue={50}
+        value={value}
+        onChange={(event, v) => {
+          setValue(v);
+        }}
+      />
     </div>
   );
-}
+};
+export default CustomSlider;
