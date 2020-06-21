ce300aa5ad06b77606bd0a9eaed3dc850a973f70
SikandarZameer <46181276+SikandarZameer@users.noreply.github.com>
2020-06-21 22:03:50 +0500

    scroll-bar-removal

--- src/Screens/SearchScreen.jsx
+++ src/Screens/SearchScreen.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
 import { Grid, Paper, Typography } from "@material-ui/core";
 import CustomSlider from "../Components/CustomSlider";
 import CustomButton from "../Components/CustomButton";
@@ -6,57 +8,78 @@ import CustTextFields from "../Components/CustTextFields";
 import CustSelect from "../Components/CustSelect";
 import Box from "@material-ui/core/Box";
 
-const SearchScreen = () => {
-  return (
-    <Paper square elevation={0}>
-      <Grid container spacing={0} direction="row">
-        <Grid item xs={3} />
-        <Grid
-          item
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-          xs={6}
-          style={{ height: "100vh" }}
-        >
-          <Grid item xs={12} style={{ height: "20vh" }} />
-          <Grid item xs={12}>
-            <Typography display="inline" variant="h3">
-              Search
-            </Typography>
-            <Typography display="inline" variant="h3">
-              <Box display="inline" fontWeight="fontWeightBold" m={1}>
-                Doctors
-              </Box>
-            </Typography>
-          </Grid>
-          <Grid item container xs={12} spacing={1}>
-            <Grid item xs={6}>
-              <CustTextFields />
-            </Grid>
-            <Grid item xs={6}>
-              <CustSelect />
-            </Grid>
-          </Grid>
-          <Grid item xs={4}>
-            <CustomSlider label1="Price" label2="500-1000" points={[20, 50]} />
-          </Grid>
-          <Grid item xs={4}>
-            <CustomSlider label1="Distance" label2="5km" points={30} />
-          </Grid>
-          <Grid item xs={4}>
-            <CustomSlider label1="Ranking" label2="Medium" points={30} />
-          </Grid>
-          <Grid item xs={12}>
-            <CustomButton text="Search" />
-          </Grid>
-          <Grid item xs={12} style={{ height: "20vh" }} />
-        </Grid>
+const useStyles = makeStyles({
+  // root: {
+  //   height: "100vh"
+  // },
+  paper: {
+    width: "100vw",
+    height: "100vh"
+  }
+});
 
-        <Grid item xs={3} />
-      </Grid>
-    </Paper>
+const SearchScreen = () => {
+  const classes = useStyles();
+
+  return (
+    // <div className={classes.root}>
+    <>
+      <CssBaseline />
+      <Paper square elevation={0} className={classes.paper}>
+        <Grid container spacing={0} direction="row">
+          <Grid item xs={3} />
+          <Grid
+            item
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            xs={6}
+            style={{ height: "100vh" }}
+          >
+            <Grid item xs={12} style={{ height: "20vh" }} />
+            <Grid item xs={12}>
+              <Typography display="inline" variant="h3">
+                Search
+              </Typography>
+              <Typography display="inline" variant="h3">
+                <Box display="inline" fontWeight="fontWeightBold" m={1}>
+                  Doctors
+                </Box>
+              </Typography>
+            </Grid>
+            <Grid item container xs={12} spacing={1}>
+              <Grid item xs={6}>
+                <CustTextFields />
+              </Grid>
+              <Grid item xs={6}>
+                <CustSelect />
+              </Grid>
+            </Grid>
+            <Grid item xs={4}>
+              <CustomSlider
+                label1="Price"
+                label2="500-1000"
+                points={[20, 50]}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <CustomSlider label1="Distance" label2="5km" points={30} />
+            </Grid>
+            <Grid item xs={4}>
+              <CustomSlider label1="Ranking" label2="Medium" points={30} />
+            </Grid>
+            <Grid item xs={12}>
+              <CustomButton text="Search" />
+            </Grid>
+            <Grid item xs={12} style={{ height: "20vh" }} />
+          </Grid>
+
+          <Grid item xs={3} />
+        </Grid>
+      </Paper>
+    </>
+    // </div>
   );
 };
 
